9670c5cffe3b2b140fd8c81b1c7a73f04f12c828
Mahmoud Hashemi <mahmoud@hatnote.com>
2016-09-03 21:46:56 -0700

    adding a utility for generating commons urls from filenames

---
+++ montage/imgutils.py
@@ -0,0 +1,40 @@
+
+import hashlib
+
+"""
+https://upload.wikimedia.org/wikipedia/commons/8/8e/%D9%86%D9%82%D8%B4_%D8%A8%D8%B1%D8%AC%D8%B3%D8%AA%D9%87_%D8%A8%D9%84%D8%A7%D8%B4_2.JPG
+
+https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/%D9%86%D9%82%D8%B4_%D8%A8%D8%B1%D8%AC%D8%B3%D8%AA%D9%87_%D8%A8%D9%84%D8%A7%D8%B4_2.JPG/360px-%D9%86%D9%82%D8%B4_%D8%A8%D8%B1%D8%AC%D8%B3%D8%AA%D9%87_%D8%A8%D9%84%D8%A7%D8%B4_2.JPG
+"""
+
+BASE = u'https://upload.wikimedia.org/wikipedia/commons'
+
+
+def make_mw_img_url(title, size=None):
+    if isinstance(title, unicode):
+        thash = hashlib.md5(title.encode('utf8')).hexdigest()
+    elif isinstance(title, bytes):
+        thash = hashlib.md5(title).hexdigest()
+    else:
+        raise TypeError('image title must be bytes or unicode')
+
+    if size is None:
+        size = 'orig'
+    elif isinstance(size, int):
+        size = '%spx' % size
+    elif str(size).lower().startswith('sm'):
+        size = '240px'
+    elif str(size).lower().startswith('med'):
+        size = '480px'
+
+    if size != 'orig' and not str(size).endswith('px'):
+        raise ValueError('size expected one of "sm", "med", "orig",'
+                         ' or an integer pixel value, not %r' % size)
+
+    if size == 'orig':
+        parts = [BASE, thash[:1], thash[:2], title]
+    else:
+        parts = [BASE, 'thumb', thash[:1], thash[:2],
+                 title, '%s-%s' % (size, title)]
+
+    return u'/'.join(parts)
