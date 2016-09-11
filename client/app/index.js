04886baac9bfcd515be777d0f0c6160829c8e5ff
Paweł Marynowski <pawel.marynowski@solution4future.com>
2016-09-11 15:44:41 +0200

    feat(app): add ui-router

--- client/app/index.js
+++ client/app/index.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import ngMaterial from 'angular-material';
+import uiRouter from 'angular-ui-router';
 
 import './style.scss';
 import 'angular-material/angular-material.css';
@@ -8,16 +9,27 @@ import 'material-design-icons/iconfont/material-icons.css';
 import components from './components';
 import services from './services';
 
-angular.module('app', ['ngMaterial'])
-  .config(['$mdThemingProvider', '$provide', function($mdThemingProvider, $provide) {
-      $mdThemingProvider.generateThemesOnDemand(true);
-      $mdThemingProvider.alwaysWatchTheme(true);
-      $provide.value('themeProvider', $mdThemingProvider);
-  }]);
+angular.module('montage', ['ngMaterial', 'ui.router'])
+  .config(function ($mdThemingProvider, $provide, $stateProvider) {
+    $mdThemingProvider.generateThemesOnDemand(true);
+    $mdThemingProvider.alwaysWatchTheme(true);
+    $provide.value('themeProvider', $mdThemingProvider);
+
+    // states
+    [
+      {
+        name: 'hello',
+        url: '/hello',
+        template: '<hello name="\'Edward\'"></hello>'
+      }
+    ].forEach(function (state) {
+      $stateProvider.state(state);
+    });
+  });
 
 const MainComponent = {
   bindings: {},
-  controller: function($scope, $mdTheming, dataService, versionService) {
+  controller: function ($scope, $mdTheming, dataService, versionService) {
     let vm = this;
 
     versionService.setVersion('gray');
@@ -29,10 +41,13 @@ const MainComponent = {
         </md-button>
         <h2>montage</h2>
         <span flex></span>
+        <span>
+          <a ui-sref="hello" ui-sref-active="active">Hello</a>
+         </span>
       </div>
     </md-toolbar>
-    <div class="container" layout="row">
-      <hello name="'World'"></hello>
+    <div class="container" layout="row" flex>
+      <ui-view></ui-view>
     </div>`
 };
 
