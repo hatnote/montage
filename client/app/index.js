cc6cb627b336e48a5aeccba7aa63083ce6c28e4b
Paweł Marynowski <yarl@o2.pl>
2016-09-12 23:43:01 +0200

    fix(app): rewrite routing, add link to dahsboard

--- client/app/index.js
+++ client/app/index.js
@@ -10,34 +10,44 @@ import components from './components';
 import services from './services';
 
 angular.module('montage', ['ngMaterial', 'ui.router'])
-  .config(function ($mdThemingProvider, $provide, $stateProvider) {
+  .config(function ($mdThemingProvider, $provide, $stateProvider, $urlRouterProvider) {
     $mdThemingProvider.generateThemesOnDemand(true);
     $mdThemingProvider.alwaysWatchTheme(true);
     $provide.value('themeProvider', $mdThemingProvider);
 
-    // states
-    [
-      {
-        name: 'dashboard',
-        url: '/dashboard',
-        template: '<mont-dashboard layout="column" layout-align="start start" data="$resolve.campaigns"></mont-dashboard>',
+    $stateProvider
+      .state('main', {
+        abstract: true,
+        template: '<ui-view/>',
         resolve: {
-          campaigns: (userService) => userService.getCampaigns()
+          campaigns: function ($state, $timeout, userService) {
+            return userService.getCampaigns().then(data => {
+              if (data.data.status === 'failure') {
+                $timeout(() => $state.go('login'));
+                return false;
+              } else {
+                return data.data.data;
+              }
+            });
+          }
         }
-      },
-      {
-        name: 'hello',
-        url: '/hello',
-        template: '<hello name="\'Edward\'"></hello>'
-      },
-      {
-        name: 'login',
+      })
+      .state('main.dashboard', {
+        url: '/',
+        template: '<mont-dashboard layout="column" layout-align="start start" data="$resolve.campaigns"></mont-dashboard>'
+      })
+      .state('main.round', {
+        url: '/round/:id',
+        template: '<mont-round layout="column" layout-align="start start" data="$resolve.round"></mont-round>',
+        resolve: {
+          round: ($stateParams, userService) => userService.getRound($stateParams.id)
+        }
+      })
+      .state('login', {
         url: '/login',
         template: '<mont-login layout="column" layout-align="center center"></mont-login>'
-      }
-    ].forEach(function (state) {
-      $stateProvider.state(state);
-    });
+      });
+    $urlRouterProvider.otherwise('/');
   });
 
 const MainComponent = {
@@ -52,7 +62,7 @@ const MainComponent = {
         <md-button class="md-icon-button" aria-label="Settings" ng-disabled="true">
           <md-icon>menu</md-icon>
         </md-button>
-        <h2>montage</h2>
+        <h2 ui-sref="main.dashboard">montage</h2>
         <span flex></span>
       </div>
     </md-toolbar>
