4c07197ace71b6437a61668218320aa9ca31efee
Paweł Marynowski <yarl@o2.pl>
2016-09-12 23:43:44 +0200

    fix(services): clean-up and fix services

--- client/app/services/userService.js
+++ client/app/services/userService.js
@@ -3,7 +3,7 @@ const UserService = function($http, $q) {
   const service = {
       getCampaign: (id) => $http.get('/admin/' + id),
       getCampaigns: () => $http.get('/admin'),
-      getRound: (id) => $http.get('/admin/round' + id)
+      getRound: (id) => $http.get('/admin/round/' + id)
   };
 
   return service;
