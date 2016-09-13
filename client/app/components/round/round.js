3158a6bdd409b8e6db611ce4b47573985a8673b0
Paweł Marynowski <yarl@o2.pl>
2016-09-14 00:41:42 +0200

    feat(round): init image gallery

--- client/app/components/round/round.js
+++ client/app/components/round/round.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 import './round.scss';
 import template from './round.tpl.html';
 
@@ -7,7 +9,30 @@ const RoundComponent = {
     },
     controller: function ($state) {
         let vm = this;
-        vm.round = vm.data.data.data;
+        vm.round = vm.data.data; //vm.data.data.data;
+        vm.user = vm.round.user;
+
+        vm.isJuror = isJuror(vm.user.id);
+        vm.setGallerySize = (size) => { vm.size = size; };
+        vm.size = 1;
+
+        // temp image generator
+
+        vm.images = [];
+        for (let i = 0; i < 50; i++) {
+            vm.images.push({
+                title: 'Image ' + i
+            });
+        }
+
+        // functions
+
+        function isJuror(id) {
+            return !!_.find(vm.round.jurors, {
+                active: true,
+                id: id
+            });
+        }
     },
     template: template
 };
