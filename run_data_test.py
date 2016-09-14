c0e05d07f143c618996dd05ab6cf97fa3a766aba
Mahmoud Hashemi <mahmoud@hatnote.com>
2016-09-14 02:09:36 -0700

    basic arguments for run_data_test.py

--- run_data_test.py
+++ run_data_test.py
@@ -1,5 +1,6 @@
 
 import os.path
+import argparse
 
 from montage.rdb import User, Campaign, Round, Base, create_initial_tasks
 from montage.loaders import load_full_csv
@@ -7,8 +8,10 @@ from montage.loaders import load_full_csv
 CUR_PATH = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(CUR_PATH, 'test_data')
 
+DEFAULT_DB_URL = 'sqlite:///tmp_montage.db'
 
-def make_rdb_session(db_url='sqlite:///tmp_montage.db', echo=True):
+
+def make_rdb_session(db_url=DEFAULT_DB_URL, echo=True):
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
 
@@ -22,8 +25,8 @@ def make_rdb_session(db_url='sqlite:///tmp_montage.db', echo=True):
     return session
 
 
-def make_fake_data(debug=True, echo=True):
-    rdb_session = make_rdb_session(echo=echo)
+def populate_initial_data(db_url, debug=False, echo=True):
+    rdb_session = make_rdb_session(db_url=db_url, echo=echo)
     coord = User(username='Leila')
     juror1 = User(username='Slaporte')
     juror2 = User(username='MahmoudHashemi')
@@ -47,10 +50,12 @@ def make_fake_data(debug=True, echo=True):
 
     for entry in entries:
         round.entries.append(entry)
+    print ' + loaded %s entries' % len(entries)
     rdb_session.commit()
 
     tasks = create_initial_tasks(rdb_session, round)
     rdb_session.add_all(tasks)
+    print ' + assigned %s tasks' % len(tasks)
 
     rdb_session.commit()
     if debug:
@@ -59,7 +64,23 @@ def make_fake_data(debug=True, echo=True):
 
 
 def main():
-    make_fake_data(True, False)
+    prs = argparse.ArgumentParser('create montage db and load initial data')
+    add_arg = prs.add_argument
+    add_arg('--db_url', default=DEFAULT_DB_URL)
+    add_arg('--debug', action="store_true", default=False)
+    add_arg('--verbose', action="store_true", default=False)
+
+    args = prs.parse_args()
+
+    try:
+        populate_initial_data(echo=args.verbose,
+                              debug=args.debug,
+                              db_url=args.db_url)
+    except Exception:
+        if not args.debug:
+            raise
+        import pdb;pdb.post_mortem()
+
     return
 
 
