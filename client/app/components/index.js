11fec370705b16219688ab15b5147a926041fafb
Paweł Marynowski <yarl@o2.pl>
2016-09-14 21:47:29 +0200

    fix(campaign): import campaign, nuke test component

--- client/app/components/index.js
+++ client/app/components/index.js
@@ -1,10 +1,12 @@
 import main from './main/main';
+import campaign from './campaign/campaign';
 import dashboard from './dashboard/dashboard';
 import round from './round/round';
 import login from './login/login';
 
 export default () => {
   main();
+  campaign();
   dashboard();
   round();
   login();
