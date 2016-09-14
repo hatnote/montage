f99c7a18002707a49c36903c4c736fefa8ee0c6f
Paweł Marynowski <yarl@o2.pl>
2016-09-15 00:57:55 +0200

    feat(round): add image dialog

--- client/app/components/round/round.js
+++ client/app/components/round/round.js
@@ -2,13 +2,14 @@ import _ from 'lodash';
 
 import './round.scss';
 import template from './round.tpl.html';
+import imageTemplate from './image.tpl.html';
 
 const RoundComponent = {
     bindings: {
         data: '<',
         images: '<' // temporary!
     },
-    controller: function ($state) {
+    controller: function ($state, $mdDialog) {
         let vm = this;
         vm.round = vm.data.data; //vm.data.data.data;
         vm.user = vm.round.user;
@@ -16,8 +17,10 @@ const RoundComponent = {
         vm.isJuror = isJuror(vm.user.id);
         vm.setGallerySize = (size) => { vm.size = size; };
         vm.size = 1;
+        vm.openImage = openImage;
 
         // functions
+
         vm.images = vm.images.data.images.map(element => ({
             id: element.id,
             title: element.title,
@@ -28,6 +31,23 @@ const RoundComponent = {
             return !!_.find(vm.round.jurors, {
                 active: true,
                 id: id
+            });
+        }
+
+        function openImage(image, event) {
+            $mdDialog.show({
+                parent: angular.element(document.body),
+                targetEvent: event,
+                clickOutsideToClose: true,
+                template: imageTemplate,
+                controller: ($scope, $mdDialog) => {
+                    $scope.cancel = () => $mdDialog.hide();
+                    $scope.image = image;
+                }
+            }).then(function(answer) {
+                // answer
+            }, function() {
+                // cancelled
             });
         }
     },
