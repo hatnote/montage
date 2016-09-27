365530d84978963c03622dd843414ff0af082170
Paweł Marynowski <yarl@o2.pl>
2016-09-27 23:31:06 +0200

    feat(dashboard): adding new campaign

--- client/app/components/dashboard/dashboard.js
+++ client/app/components/dashboard/dashboard.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 
 import './dashboard.scss';
 import template from './dashboard.tpl.html';
+import templateNewCampaign from './new-campaign.tpl.html';
 
 const DashboardComponent = {
     bindings: {
@@ -9,8 +10,9 @@ const DashboardComponent = {
         user: '=',
         type: '<'
     },
-    controller: function ($state, userService, versionService) {
+    controller: function ($filter, $state, $mdDialog, userService, versionService) {
         let vm = this;
+        vm.addCampaign = addCampaign;
         vm.isAdmin = isAdmin;
         vm.campaigns = isAdmin() ? vm.data.data : _.groupBy(vm.data.data, 'campaign.id');
         vm.logout = logout;
@@ -21,6 +23,52 @@ const DashboardComponent = {
         versionService.setVersion(isAdmin() ? 'grey' : 'blue');
 
         // functions 
+
+        function addCampaign(event) {
+            $mdDialog.show({
+                template: templateNewCampaign,
+                parent: angular.element(document.body),
+                targetEvent: event,
+                clickOutsideToClose: false,
+                controller: ($scope, $mdDialog, $timeout, dataService) => {
+                    $scope.campaign = {
+                        name: ''
+                        //coordinators: []
+                    };
+                    $scope.searchUser = (searchName) => dataService.searchUser(capitalize(searchName)).then((response) => {
+                        return response.data.query.globalallusers;
+                    });
+                    $scope.create = function () {
+                        let campaign = angular.copy($scope.campaign);
+                        /*
+                        campaign = angular.extend(campaign, {
+                            coordinators: campaign.coordinators.map((element) => element.name).join(',')
+                        });
+                        */
+                        
+                        $scope.loading = true;
+                        userService.admin.addCampaign(campaign).then((response) => {
+                            $scope.loading = false;
+                            $mdDialog.hide(true);
+                            $state.reload();
+                        }, (response) => {
+                            $scope.loading = false;
+                            console.log('err', response);
+                        });
+                    };
+                    $scope.hide = function () {
+                        $mdDialog.hide();
+                    };
+                    $scope.cancel = function () {
+                        $mdDialog.cancel();
+                    };
+                }
+            });
+        }
+
+        function capitalize(text) {
+            return text.charAt(0).toUpperCase() + text.slice(1);
+        }
 
         function isAdmin() {
             return vm.type === 'admin';
