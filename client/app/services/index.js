b33993e297881c147ac5d30e161a74f0ef8d5218
Paweł Marynowski <yarl@o2.pl>
2016-09-28 23:15:02 +0200

    feat(services): add alert service

--- client/app/services/index.js
+++ client/app/services/index.js
@@ -1,8 +1,10 @@
+import alertService from './alertService';
 import dataService from './dataService';
 import userService from './userService';
 import versionService from './versionService';
 
 export default () => {
+  alertService();
   dataService();
   userService();
   versionService();
