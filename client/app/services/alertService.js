90a309f24c0dbf15db17d10a0e8e8c52dc1804a2
Paweł Marynowski <yarl@o2.pl>
2016-10-05 23:58:29 +0200

    feat(services): add editable hide delay to toast service

--- client/app/services/alertService.js
+++ client/app/services/alertService.js
@@ -1,16 +1,19 @@
 const AlertService = function ($mdToast) {
 
+    let toast = $mdToast.simple()
+        .highlightClass('md-accent')
+        .toastClass('toast__top')
+        .position('bottom left');
+
     const service = {
-        success: (text) => $mdToast.show($mdToast.simple()
-            .textContent(text)
-            .highlightClass('md-accent')
-            .toastClass('toast__top')
-            .position('bottom left')),
-        error: (error) => $mdToast.show($mdToast.simple()
-            .textContent(error.message + ': ' + error.detail)
-            .highlightClass('md-accent')
-            .toastClass('toast__top')
-            .position('bottom left'))
+        success: (text, time) => {
+            toast.textContent(text).hideDelay(time || 1000);
+            $mdToast.show(toast);
+        },
+        error: (error, time) => {
+            toast.textContent(error.message + ': ' + error.detail).hideDelay(time || 1000);
+            $mdToast.show(toast);
+        }
     };
 
     return service;
