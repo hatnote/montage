30774e6d7a6027d46db2c5fe38bab8b11703a142
Mahmoud Hashemi <mahmoud@hatnote.com>
2016-10-13 21:27:17 -0700

    adding frontend logging endpoint, configure feel_log_path to enable, check the updated config gist for an example

--- montage/meta_endpoints.py
+++ montage/meta_endpoints.py
@@ -1,6 +1,10 @@
 
-from clastic import render_basic
+import json
+import datetime
+
+from clastic import render_basic, GET, POST
 from clastic.errors import Forbidden
+from boltons.strutils import indent
 from boltons.jsonutils import reverse_iter_lines
 
 
@@ -9,7 +13,9 @@ DEFAULT_LINE_COUNT = 500
 
 def get_meta_routes():
     ret = [('/logs/api', get_api_log_tail, render_basic),
-           ('/logs/api_exc', get_api_exc_log_tail, render_basic)]
+           ('/logs/api_exc', get_api_exc_log_tail, render_basic),
+           GET('/logs/feel', get_frontend_error_log, render_basic),
+           POST('/logs/feel', post_frontend_error_log, render_basic)]
     return ret
 
 
@@ -53,6 +59,30 @@ def _get_tail_from_path(path, count=DEFAULT_LINE_COUNT):
         lines.append(line)
     lines.reverse()
     return lines
+
+
+def post_frontend_error_log(user, config, request_dict):
+    feel_path = config.get('feel_log_path', None)
+    if not feel_path:
+        return ['(no front-end error log configured)']
+    now = datetime.datetime.utcnow()
+    now_str = now.isoformat()
+
+    username = user.username if user else '<nouser>'
+    err_bytes = json.dumps(request_dict, sort_keys=True, indent=2)
+    err_bytes = indent(err_bytes, '  ')
+    with open(feel_path, 'ab') as feel_file:
+        feel_file.write('Begin error at %s:\n\n' % now_str)
+        feel_file.write('  + Username: ' + username + '\n')
+        feel_file.write(err_bytes)
+        feel_file.write('\n\nEnd error at %s\n\n' % now_str)
+
+    return
+
+
+def get_frontend_error_log(config, request_dict):
+    # TODO
+    return []
 
 
 META_ROUTES = get_meta_routes()
