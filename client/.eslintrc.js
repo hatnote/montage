cd1ad0ea8a13fd038453fffe97202a101f046818
Paweł Marynowski <yarl@o2.pl>
2017-09-07 19:54:59 +0200

    chore(app): update angular, install eslint

---
+++ client/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "module"
+  },
+  "extends": ["airbnb", "angular"],
+  "rules": {
+    "no-param-reassign": [2, { "props": false }]
+  }
+}
