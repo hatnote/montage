d8b9747cd413ea08579881768d4b618f4d6b0008
Paweł Marynowski <yarl@o2.pl>
2017-09-14 00:05:19 +0200

    feat(main): add wide container

--- client/src/components/main/main.js
+++ client/src/components/main/main.js
@@ -5,6 +5,18 @@ const Component = {
   template,
 };
 
-function controller() {}
+function controller($state) {
+  const vm = this;
+
+  vm.isLinkActive = isLinkActive;
+
+  function isLinkActive(states) {
+    const list = angular.isArray(states)
+      ? states
+      : [states];
+
+    return list.some(stateName => $state.includes(stateName));
+  }
+}
 
 export default Component;
