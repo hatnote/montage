92f30940b4e0c8b7632e690be719b5bf849e7e88
Paweł Marynowski <yarl@o2.pl>
2017-09-14 00:01:37 +0200

    feat(campaign): add campaign features

--- client/src/components/campaign/campaign.component.js
+++ client/src/components/campaign/campaign.component.js
@@ -10,6 +10,7 @@ function controller($stateParams, adminService) {
   const vm = this;
 
   vm.campaign = null;
+  vm.dict = {};
 
   vm.activateRound = activateRound;
   vm.pauseRound = pauseRound;
@@ -20,6 +21,7 @@ function controller($stateParams, adminService) {
 
   vm.$onInit = () => {
     const id = $stateParams.id.split('-')[0];
+
     adminService
       .getCampaign(id)
       .then((data) => {
@@ -30,6 +32,22 @@ function controller($stateParams, adminService) {
         });
       })
       .catch((err) => { vm.error = err.data; });
+
+    vm.dict.settings = {
+      allowed_filetypes: 'Allowed filetypes',
+      dq_by_filetype: 'Disqualify by filetype',
+      dq_by_resolution: 'Disqualify by resolution',
+      dq_by_upload_date: 'Disqualify by upload date',
+      dq_by_uploader: 'Disqualify by uploader',
+      dq_coords: 'Disqualify by coordinates',
+      dq_maintainers: 'Disqualify maintainers',
+      dq_organizers: 'Disqualify maintainers',
+      final_threshold: 'Final threshold',
+      min_resolution: 'Min. resolution',
+      show_filename: 'Show filename',
+      show_link: 'Show link',
+      show_resolution: 'Show resolution',
+    };
   };
 
   function activateRound(round) {
