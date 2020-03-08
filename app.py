a20c9c0763329ce00a6df581acbd7e117a7c055c
Mahmoud Hashemi <mahmoud@hatnote.com>
2020-03-07 18:59:13 -0800

    couple of updates for wmflabs integration

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
 # this file is only used by wmflabs for hosting
-from montage.server import app
+from montage.app import create_app
+from montage.utils import get_env_name
+
+env_name = get_env_name()
+app = create_app(env_name=env_name)
