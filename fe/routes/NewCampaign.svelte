b262b74a9f2942aade46fc2b6b44262821197601
Stephen LaPorte <stephen.laporte@gmail.com>
2020-03-08 11:48:59 -0700

    add more wip post functionality

--- fe/routes/NewCampaign.svelte
+++ fe/routes/NewCampaign.svelte
@@ -2,9 +2,20 @@
   <Title>Create a new campaign</Title>
   <Content>
     <h2>Basic information</h2>
+    <div>
+      <Select enhanced bind:value={series_choice} label="Series">
+      <MontageAPI api='/v1/series' let:data={serieses}>
+        {#each serieses as series}
+          <Option value={series.id}>{series.name}</Option>
+        {/each}
+        <Option value="new">New Series</Option>
+      </MontageAPI>
+      </Select>
+    </div>
+
     <Textfield
       fullwidth
-      value=""
+      bind:value={name}
       label="Name"
       input$aria-controls="helper-text"
       input$aria-describedby="helper-text" />
@@ -12,7 +23,7 @@
 
     <Textfield
       fullwidth
-      value=""
+      bind:value={url}
       label="Campaign URL"
       input$aria-controls="helper-text"
       input$aria-describedby="helper-text" />
@@ -21,24 +32,16 @@
     <div>
       <h2>Date range</h2>
       <p>Select the start and end date for the competition you will be judging. When you import photos, you can disqualify photos that were not uploaded during this range.</p>
-      <Textfield value="" label="Start date" type="date" />
-      <Textfield value="" label="End date" type="date" />
+      <Textfield bind:value={startDate} label="Start date" type="date" />
+      <Textfield bind:value={endDate} label="End date" type="date" />
     </div>
     <h2>Coordinators</h2>
     <p>Coordinators are people who have access to editing the campaign and rounds, and viewing voting statistics.</p>
 
-
-    <Set bind:chips={coordinators} chips={coordinators} let:chip input>
-      <Chip><Text>{chip}</Text><Icon class="material-icons" trailing tabindex="0">cancel</Icon></Chip>
-    </Set>
-
-
     <Textfield
       bind:value={search_name}
       on:keyup={({ target: { value } }) => debounce(search_name)}
-      label="Name"
-      input$aria-controls="helper-text"
-      input$aria-describedby="helper-text" />
+      label="Name"/>
 
       {#await confirmed_username}
       Confirming...
@@ -50,32 +53,84 @@
         {/if}
       {/await}
 
+    <Set chips={coordinators} let:chip input>
+      <Chip><Text>{chip}</Text><Icon class="material-icons" trailing tabindex="0">cancel</Icon></Chip>
+    </Set>
+    <h2>Next</h2>
+    <p>After you create the campaign, you will create and add photos to the first round.</p>
   </Content>
 </Paper>
+<div style="display: flex; align-items: center; flex-wrap: wrap;">
+  <Button variant="raised" float="right" style="width:100%" on:click|once={addCampaign}>
+    <Icon class="material-icons">add</Icon>
+    <Label>Create</Label>
+  </Button>
+</div>
 
-
+{#if post_promise}
+  {#await post_promise}
+  ... creating campaign
+  {:then post_data}
+    Created: {post_data.data.name}
+  {/await}
+{/if}
 
 <script>
+  import Select, {Option} from '@smui/select';
   import Textfield, {Input, Textarea} from '@smui/textfield';
   import Paper, {Title, Subtitle, Content} from '@smui/paper';
   import HelperText from '@smui/textfield/helper-text/index';
   import Button, {Label} from '@smui/button';
-  import Chip, {Set, Icon, Checkmark, Text} from '@smui/chips';
+  import Chip, {Set, Icon, Text} from '@smui/chips';
+
+  import MontageAPI from '../MontageAPI.svelte'
+
+  let post_promise;
+
+  // post data
+  let series;
+  let name = '';
+  let url = '';
+  let startDate = ''; // this should default to september 1 the previous year
+  let endDate = ''; // this should probably default to september 30 the previous year
+  let coordinators = [];
+
+  async function addCampaign() {
+    const api_url = '/v1/admin/add_campaign'
+    const settings = {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          'name': name,
+          'coordinators': coordinators,
+          'open_date': startDate,
+          'close_date': endDate,
+          'url': url,
+          'series_id': series_choice
+        })
+    }
+    const resp = await fetch(api_url, settings)
+    const data = await resp.json()
+    post_promise = data
+    return data
+  }
 
 
   let confirmed_username;
-  let chipSet;
   let search_name = '';
-  let coordinators = ['test'];
 
-
-
-
-
+  let series_choice;
 
   function addName() {
-    coordinators = [...coordinators, search_name];
-    console.log(coordinators)
+    if(!coordinators.includes(search_name)) {
+      coordinators = [...coordinators, search_name];
+    } else {
+      console.log('already have it!')
+      console.log(coordinators)
+    }
   }
 
   let debounced_name='';
