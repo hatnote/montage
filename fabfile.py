ad5370649077261308eb2d9483e6a1416683371d
Mahmoud Hashemi <mahmoud@hatnote.com>
2020-03-10 23:53:46 -0700

    handle some deploy script exit behavior

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,7 @@
 
+import sys
 import time
+import atexit
 import threading
 
 from fabric.api import env, run, sudo, cd, local
@@ -13,11 +15,50 @@ SHELL_POD_NAME = 'interactive'  # this'll only change if the webservce command d
 TOOL_NAME = 'montage-dev'
 TOOL_IMAGE = 'python2'
 
+
+# if you hit ctrl-c while ssh'd in, it kills the session, and if you
+# were in the middle of pip installing packages, it will leave the
+# interactive shell around. so we use atexit to clean up. (also gotta
+# clean up stdin termios)
+_SHELL_UP = False
+
+old_termios_attrs = None
+try:
+    import termios
+    import tty
+    old_termios_attrs = termios.tcgetattr(sys.stdin)
+except:
+    pass
+
+def _shutdown_shell():
+    if _SHELL_UP:
+        # host string has to be reset for some unknown reason.
+        env.host_string = 'login.tools.wmflabs.org'
+        env.sudo_prefix = "sudo -ni -p '%(sudo_prompt)s' "
+        sudo('kubectl delete pod %s' % SHELL_POD_NAME)
+    if old_termios_attrs is not None:
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_termios_attrs)
+    return
+
+atexit.register(_shutdown_shell)
+
+
 def deploy():
+    cur_branch = local('git symbolic-ref --short HEAD', capture=True).stdout.strip()
 
-    res = local('git --no-pager log origin/{0}...{0}'.format(RELEASE_BRANCH_NAME))
-    assert res.stdout == '', 'unpushed/unpulled commits on %s, run git push, test, and try again' % RELEASE_BRANCH_NAME
+    print()
+    print(' ++ deploying %s to %s' % (RELEASE_BRANCH_NAME, TOOL_NAME))
+    if cur_branch != RELEASE_BRANCH_NAME:
+        print(' -- note that you are on %s' % cur_branch)
+        time.sleep(3)
+    print()
 
+    res = local('git --no-pager log origin/{0}...{0}'.format(RELEASE_BRANCH_NAME), capture=True)
+    if res.stdout != '':
+        raise SystemExit(' !! unpushed/unpulled commits on release branch %s,'
+                         ' run git push, test, and try again.' % RELEASE_BRANCH_NAME)
+
+    time.sleep(3)
     result = run('whoami')
     prefix = run('cat /etc/wmflabs-project').stdout.strip()
 
@@ -33,14 +74,22 @@ def deploy():
 
 
     def _webservice_shell_steps():
+        global _SHELL_UP
+
         time.sleep(10)
         pip_upgrade_cmd = ("kubectl exec " + SHELL_POD_NAME +
                            " -- www/python/venv/bin/pip install --upgrade -r montage/requirements.txt")
-        sudo(pip_upgrade_cmd)
+        _SHELL_UP = True
+
+        out = sudo(pip_upgrade_cmd)
         sudo('kubectl delete pod %s' % SHELL_POD_NAME)
 
-    # couldn't figure out a non-brittle* way of starting up a shell
-    # pod in the background. so, we background it here and sleep to
+        _SHELL_UP = False
+
+        return
+
+    # needed a non-brittle* way of starting up a shell
+    # pod in the background. so, we background it here, and sleep to
     # give time for the pod to come up and get deleted.
     #
     # *non-brittle = not reimplementing the internals of the
@@ -52,6 +101,7 @@ def deploy():
     time.sleep(3)
 
     sudo("webservice %s restart" % TOOL_IMAGE)
+
 
 
 # $(kubectl get pods -l name=montage-dev -o jsonpath='{.items[0].metadata.name}')
