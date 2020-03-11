b504daab2cacc9985e48ec9e90d69a8b255bf9f5
Mahmoud Hashemi <mahmoud@hatnote.com>
2020-03-10 21:44:11 -0700

    factor out some knobs and add check for being fully pushed

--- fabfile.py
+++ fabfile.py
@@ -2,48 +2,56 @@
 import time
 import threading
 
-from fabric.api import env, run, sudo, cd
+from fabric.api import env, run, sudo, cd, local
 
 env.hosts = ['login.tools.wmflabs.org']
 env.sudo_prefix = "sudo -ni -p '%(sudo_prompt)s' "
 
-# $(kubectl get pods -l name=montage-dev -o jsonpath='{.items[0].metadata.name}')
-
+# TODO
+RELEASE_BRANCH_NAME = 'master'
+SHELL_POD_NAME = 'interactive'  # this'll only change if the webservce command does
+TOOL_NAME = 'montage-dev'
+TOOL_IMAGE = 'python2'
 
 def deploy():
+
+    res = local('git --no-pager log origin/{0}...{0}'.format(RELEASE_BRANCH_NAME))
+    assert res.stdout == '', 'unpushed/unpulled commits on %s, run git push, test, and try again' % RELEASE_BRANCH_NAME
+
     result = run('whoami')
     prefix = run('cat /etc/wmflabs-project').stdout.strip()
 
-    # TODO: control env here
-    username = '%s.%s' % (prefix, 'montage-dev')
+    username = '%s.%s' % (prefix, TOOL_NAME)
     env.sudo_user = username
 
     result = sudo('whoami')
+    assert result == username
 
     with cd('montage'):
-        out = sudo('git pull')
-        # i think the following might be redundant with fabric's own behavior
-        assert out.succeeded, 'git pull failed'
-        assert out.stderr == '', 'stderr present: %s' % out.stderr
+        out = sudo('git checkout %s' % RELEASE_BRANCH_NAME)
+        out = sudo('git pull origin %s' % RELEASE_BRANCH_NAME)
 
 
-    def _interactive_steps():
+    def _webservice_shell_steps():
         time.sleep(10)
-        pip_upgrade_cmd = ("kubectl exec interactive"
+        pip_upgrade_cmd = ("kubectl exec " + SHELL_POD_NAME +
                            " -- www/python/venv/bin/pip install --upgrade -r montage/requirements.txt")
         sudo(pip_upgrade_cmd)
-        sudo('kubectl delete pod interactive')
+        sudo('kubectl delete pod %s' % SHELL_POD_NAME)
 
     # couldn't figure out a non-brittle* way of starting up a shell
     # pod in the background. so, we background it here and sleep to
     # give time for the pod to come up and get deleted.
-
+    #
     # *non-brittle = not reimplementing the internals of the
     # webservice command
-    th = threading.Thread(target=_interactive_steps)
+    th = threading.Thread(target=_webservice_shell_steps)
     th.start()
 
-    sudo('webservice python2 shell', pty=True)
+    sudo('webservice %s shell' % TOOL_IMAGE, pty=True)
     time.sleep(3)
 
-    sudo("webservice python2 restart")
+    sudo("webservice %s restart" % TOOL_IMAGE)
+
+
+# $(kubectl get pods -l name=montage-dev -o jsonpath='{.items[0].metadata.name}')
