1be5d09ff5b80e2a57eb71802096fc1fcb98e60f
Mahmoud Hashemi <mahmoud@hatnote.com>
2021-10-04 17:30:44 -0700

    work around the lets encrypt issue https://phabricator.wikimedia.org/T291387

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 # this file is only used by wmflabs for hosting
+
+import urllib3.contrib.pyopenssl
+urllib3.contrib.pyopenssl.inject_into_urllib3()
+
 from montage.app import create_app
 from montage.utils import get_env_name
 
