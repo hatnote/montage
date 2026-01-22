21b0fb41b272301ed100d2df2dbd77a24704088a
itsabhinavm <abhinav051017@gmail.com>
2026-01-23 00:38:08 +0530

    feat: add alembic for db migrations

---
+++ alembic/versions/eee24470f45a_initial_migration.py
@@ -0,0 +1,24 @@
+"""Initial migration
+
+Revision ID: eee24470f45a
+Revises: 
+Create Date: 2026-01-22 18:36:19.412796
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'eee24470f45a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
