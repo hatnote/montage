472fe6d48b66a381f64685c29786007e65261178
Prity Kumari <230903586+QuantumPrity@users.noreply.github.com>
2026-03-10 21:39:39 +0530

    Fix #18: Show round directions in juror voting interface

--- frontend/src/components/Vote/Vote.vue
+++ frontend/src/components/Vote/Vote.vue
@@ -8,11 +8,35 @@
   </div>
 
   <template v-else>
+    <div v-if="round.directions" class="directions-container">
+      <p><strong>{{ $t('montage-round-directions') }}</strong></p>
+      <p>{{ round.directions }}</p>
+    </div>
     <vote-yes-no v-if="round.vote_method === 'yesno'" :round="round" :tasks="tasks" />
     <vote-rating v-else-if="round.vote_method === 'rating'" :round="round" :tasks="tasks" />
     <vote-ranking v-else-if="round.vote_method === 'ranking'" :round="round" :tasks="tasks" />
-  </template>
-</template>
+    </template>
+    
+    <script>
+    export default {
+    
+    }
+    </script>
+    
+    <style>
+    
+    </style>
+    </template>
+    
+    <script>
+    export default {
+    
+    }
+    </script>
+    
+    <style>
+    
+    </style>
 
 <script setup>
 import { ref, onMounted } from 'vue'
